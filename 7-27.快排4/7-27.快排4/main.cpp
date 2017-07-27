03e42d37e0720fd8fdc022a479964c5a0fdceb03
mihuaz2Z <mihuayouxi@163.com>
2017-07-27 10:05:46 +0800

    一次过~哈哈，好开心

---
+++ 7-27.快排4/7-27.快排4/main.cpp
@@ -0,0 +1,54 @@
+//
+//  main.cpp
+//  7-27.快排4
+//
+//  Created by Ruiwen Feng on 2017/7/27.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+void quickSort(vector<int>*nums,int low,int high){
+    if (low >= high) {
+        return;
+    }
+    
+    int key = (*nums)[low];
+    int left = low;
+    int right = high;
+    
+    while (left < right) {
+        while (left < right && (*nums)[right] >= key ) {
+            right--;
+        }
+        (*nums)[left] = (*nums)[right];
+        while (left < right && (*nums)[left] <= key) {
+            left++;
+        }
+        (*nums)[right] = (*nums)[left];
+    }
+    (*nums)[left] = key;
+    
+    quickSort(nums, low, left-1);
+    quickSort(nums, left+1, high);
+}
+
+
+
+int main(int argc, const char * argv[]) {
+   
+    vector<int> nums = {1,2,3,1,2,3};
+    
+    quickSort(&nums, 0,(int)nums.size()-1);
+    
+    for (int i = 0; i < nums.size(); i ++) {
+        cout << nums[i] << " ";
+    }
+    
+    return 0;
+}
