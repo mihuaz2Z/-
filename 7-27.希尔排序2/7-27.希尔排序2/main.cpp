08360da53b7347489f5e09e63ceb642db726ae23
mihuaz2Z <mihuayouxi@163.com>
2017-07-27 18:11:10 +0800

    居然又错了一个位置

---
+++ 7-27.希尔排序2/7-27.希尔排序2/main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  7-27.希尔排序2
+//
+//  Created by Ruiwen Feng on 2017/7/27.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#include <iostream>
+
+
+
+void shellSort(int a[],int num) {
+
+    for (int step = num/2; step > 0; step/=2) {
+        for (int i = step; i < num; i += step) {
+            for (int j = i-step; j>0 && a[j] > a[j+step]; j-=step) {
+                int temp = a[j];
+                a[j] = a[j+step];
+                a[j+step] = temp;
+            }
+        }
+    }
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    int a[] = {1,3,5,3,8,2,1,7,8,6,5};
+    
+    shellSort(a, sizeof(a)/sizeof(int));
+    
+    for (int i = 0; i < sizeof(a)/sizeof(int); i ++) {
+        std::cout<<a[i] << " ";
+    }
+    
+    
+    return 0;
+}
