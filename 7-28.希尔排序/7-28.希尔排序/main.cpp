0abdeb13787db12854f6a1e41e97498fb0adabe3
mihuaz2Z <mihuayouxi@163.com>
2017-07-28 09:39:54 +0800

    clear

---
+++ 7-28.希尔排序/7-28.希尔排序/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  7-28.希尔排序
+//
+//  Created by Ruiwen Feng on 2017/7/28.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#include <iostream>
+
+
+void shellSort(int a[],int num) {
+
+    for (int step = num/2; step > 0; step/=2) {
+        for (int i = step; i < num; i+=step) {
+            for (int j = i - step; j>=0 && a[j] > a[j+step]; j--) {
+                int temp = a[j];
+                a[j] = a[j+step];
+                a[j+step] = temp;
+            }
+        }
+    }
+    
+}
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    int a[] = {1,3,5,3,8,2,1,7,8,6,5};
+    int size = sizeof(a)/sizeof(int);
+    shellSort(a, size);
+    for (int i = 0; i < size; i ++) {
+        std::cout << a[i] << " ";
+    }
+    
+    return 0;
+}
