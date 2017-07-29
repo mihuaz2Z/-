24e10e0a92c0567dc20376535aba73697bb9ff6c
mihuaz2Z <mihuayouxi@163.com>
2017-07-29 10:16:26 +0800

    一次过，开心.

---
+++ 7-29.归并排序3/7-29.归并排序3/main.cpp
@@ -0,0 +1,68 @@
+//
+//  main.cpp
+//  7-29.归并排序3
+//
+//  Created by Ruiwen Feng on 2017/7/29.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#include <iostream>
+
+
+void mergeArray(int a[],int left,int mid,int right) {
+    
+    int i = left;
+    int j = mid+1;
+    int k = 0;
+    int temp[right-left];
+    
+    
+    while (i <= mid && j <= right) {
+        if (a[i]  <= a[j]) {
+            temp[k++] = a[i++];
+        }
+        else
+        {
+            temp[k++] = a[j++];
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = a[i++];
+    }
+
+    while (j <= mid) {
+        temp[k++] = a[j++];
+    }
+    
+    for (int m = 0; m < k; m ++) {
+        a[left+m] = temp[m];
+    }
+}
+
+void mergeSort(int a[],int left,int right) {
+    
+    if (left >= right) {
+        return;
+    }
+    
+    int mid = (left+right)/2;
+    mergeSort(a, left, mid);
+    mergeSort(a, mid+1, right);
+    
+    mergeArray(a, left, mid, right);
+}
+
+
+
+int main(int argc, const char * argv[]) {
+    int a[] = {1,3,5,3,8,2,1,7,8,6,5};
+    int size = sizeof(a)/sizeof(int);
+    mergeSort(a, 0, size-1);
+    for (int i = 0; i < size; i ++) {
+        std::cout << a[i] << " ";
+    }
+    
+    
+    return 0;
+}
