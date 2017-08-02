fc8e387e2ebd5093cd7001db3eb55aaba46e13bf
mihuaz2Z <mihuayouxi@163.com>
2017-08-02 09:30:13 +0800

    merge clear

--- 8-02.Sort/8-02.Sort/main.cpp
+++ 8-02.Sort/8-02.Sort/main.cpp
@@ -51,7 +51,43 @@ void quickSort(int a[],int low,int high) {
     
 }
 
+void mergeArray(int a[],int left,int mid,int right) {
+    int i = left;
+    int j = mid+1;
+    int temp[right-left];
+    int index = 0;
+    
+    while (i <= mid && j <= right) {
+        if (a[i] <= a[j]) {
+            temp[index++] = a[i++];
+        }
+        else {
+            temp[index++] = a[j++];
+        }
+    }
+    
+    while (i <= mid) {
+        temp[index++] = a[i++];
+    }
+    while (j <= right) {
+        temp[index++] = a[j++];
+    }
+    
+    for (int z = 0; z < index; z ++) {
+        a[left+z] = temp[z];
+    }
+    
+}
 
+void mergeSort(int a[],int left,int right) {
+    if (left >= right) {
+        return;
+    }
+    int mid = (left+right)/2;
+    mergeSort(a, left, mid);
+    mergeSort(a, mid+1, right);
+    mergeArray(a, left, mid, right);
+}
 
 
 
@@ -60,7 +96,7 @@ int main(int argc, const char * argv[]) {
     int a[] = {1,3,5,3,8,2,1,7,8,6,5};
     int size = sizeof(a)/sizeof(int);
     
-    quickSort(a,0,size-1);
+    mergeSort(a,0,size-1);
     
     for (int i = 0; i < size; i ++) {
         std::cout << a[i] << " ";
