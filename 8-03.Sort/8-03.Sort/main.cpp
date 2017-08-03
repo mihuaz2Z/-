0ac42c5dbdaadf0b3a3c7e6d541d737e128100cc
mihuaz2Z <mihuayouxi@163.com>
2017-08-03 09:07:04 +0800

    bubble clear.

---
+++ 8-03.Sort/8-03.Sort/main.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  8-03.Sort
+//
+//  Created by Ruiwen Feng on 2017/8/3.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#include <iostream>
+
+
+void swap(int*a,int*b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubble(int a[],int num) {
+    for (int i = 0; i < num; i ++) {
+        for (int j = i +1; j < num; j ++) {
+            if (a[i] > a[j]) {
+                swap(a+i, a+j);
+            }
+        }
+    }
+}
+
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    int a[] = {1,3,5,3,8,2,1,7,8,6,5};
+    int size = sizeof(a)/sizeof(int);
+    
+    bubble(a, size);
+    
+    for (int i = 0; i < size; i ++) {
+        std::cout << a[i] << " ";
+    }
+    
+    
+    
+    return 0;
+}
